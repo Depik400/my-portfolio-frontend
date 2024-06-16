c8ad12753eb49023187202ceaa1303c7303ca014
Pavel Kononov <pro100cuber@gmail.com>
2024-06-16 15:04:29 +0700

    refactoring: update eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,7 @@ module.exports = {
     rules: {
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
+        'object-curly-spacing': ['warn',  'always'],
+        'semi': ['warn', 'always'],
     }
   }
